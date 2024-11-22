330d93d458cfde37b914285eb5c8a06bcb4279d8
Denis <denis.shagim@icloud.com>
2024-11-22 17:36:47 +0500

    feat: Created the architecture

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    // Пример кастомных правил
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+import { defineConfig } from 'tailwindcss';
+
+export default defineConfig({
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('tailwindcss-logical'), // Плагин для логических стилей
-  ],
-};
+  plugins: [],
+});
